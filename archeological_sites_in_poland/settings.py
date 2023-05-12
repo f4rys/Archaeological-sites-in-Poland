5fdbe56af76f4828785c793a5193db1c55f53424
f4rys <looxxon@gmail.com>
2023-05-12 13:09:01 +0200

    Few fixes

--- archeological_sites_in_poland/settings.py
+++ archeological_sites_in_poland/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ with open(BASE_DIR / "secret_key.txt") as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://excavations.pythonanywhere.com']
+ALLOWED_HOSTS = ['excavations.pythonanywhere.com']
 
 SECURE_HSTS_SECONDS = 518400
 
@@ -128,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
