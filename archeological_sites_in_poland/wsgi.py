fd15c724dbb7ac1dd722a1e951edcbace7f89913
f4rys <looxxon@gmail.com>
2023-09-23 13:57:19 +0200

    Increased readability

--- archeological_sites_in_poland/wsgi.py
+++ archeological_sites_in_poland/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for archeological_sites_in_poland project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
