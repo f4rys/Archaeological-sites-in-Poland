6af67bb10cf65f91a3555650900c61c26f449560
f4rys <looxxon@gmail.com>
2024-07-23 12:48:32 +0200

    Rewrite for leaflet-provider

--- map/static/js/drawMap.js
+++ map/static/js/drawMap.js
@@ -1,57 +1,47 @@
 require.config({
     paths: {
-        "leaflet": "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.3/leaflet",
-        "google-mutant": "https://unpkg.com/leaflet.gridlayer.googlemutant@latest/dist/Leaflet.GoogleMutant"
+      "leaflet": "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet",
+      "leaflet-providers": "https://cdn.jsdelivr.net/npm/leaflet-providers@latest/leaflet-providers"
     },
   });
-
-define(['leaflet', 'leaflet.wms', 'google-mutant'],
-
-function(L, wms) {
-
+  
+  define(['leaflet', 'leaflet.wms', 'leaflet-providers'], function(L, wms, L_P) {
+  
     var overlayMap = createMap('overlay-map', false);
-
+  
     function createMap(div) {
-
-        var map = L.map(div);
-        map.setView([50.574283, 19.722619], 6);
-
-        var roadmap = L.gridLayer.googleMutant({
-            type: 'roadmap'
-        });
-
-        var satellite = L.gridLayer.googleMutant({
-            type: 'satellite'
-        });
-
-        var terrain = L.gridLayer.googleMutant({
-            type: 'terrain'
-        });
-
-        var hybrid = L.gridLayer.googleMutant({
-            type: 'hybrid'
-        });
-
-        var basemaps = {
-            'Google Roadmap' : roadmap.addTo(map),
-            'Google Satellite' : satellite,
-            'Google Terrain' : terrain,
-            'Google Hybrid' : hybrid
-        };
-
-        var source = wms.source(
-            "https://usluga.zabytek.gov.pl/INSPIRE_AMD/service.svc/get?",
-            {
-                "format": "image/png",
-                "transparent": "true",
-            }        
-        ).getLayer("Archaeological_Monuments").addTo(map);
-
-        L.control.layers(basemaps).addTo(map);
-
-        return map;
+  
+      var map = L.map(div);
+      map.setView([50.574283, 19.722619], 6);
+  
+      var roadmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      });
+    
+      var satellite = L.tileLayer.provider('Esri.WorldImagery');
+      var topographic = L.tileLayer.provider('OpenTopoMap');
+  
+      var basemaps = {
+        'Roadmap': roadmap,
+        'Satellite': satellite,
+        'Topographic': topographic,
+      };
+  
+      roadmap.addTo(map);
+  
+      var source = wms.source(
+        "https://usluga.zabytek.gov.pl/INSPIRE_AMD/service.svc/get?",
+        {
+          "format": "image/png",
+          "transparent": "true",
+        }
+      ).getLayer("Archaeological_Monuments").addTo(map);
+  
+      L.control.layers(basemaps).addTo(map);
+  
+      return map;
     }
-
-return overlayMap
-});
-
+  
+    return overlayMap;
+  });
+  
