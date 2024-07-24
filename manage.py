4f01aafe1e0b0b45e6ec64f5bc3f94f77cdb3e2d
f4rys <looxxon@gmail.com>
2024-07-24 14:14:11 +0200

    Update default settings file

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'archeological_sites_in_poland.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'archeological_sites_in_poland.productions_settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
